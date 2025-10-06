19cfc41ea3b0408f32a37af52c1a23216bde21c0
Oliverden3 <oliversjensen@hotmail.com>
2025-10-06 14:04:30 +0200

    Created additional projects

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+//TODO Create database schema
+//TODO Create entity for calender
+//TODO Create Model/View/Controller for calender page
+//TODO Create CRUD operations for calender entity, so that we can manipulate our calender 
